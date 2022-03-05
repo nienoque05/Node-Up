6918ecf5b2de7bcb5a37adcb2dc2525448485c46
nienoque05 <nicolasenoque0@gmail.com>
2022-03-05 12:58:03 -0300

    commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const path = require("path");
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "uploads/")
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname + Date.now()+ path.extname(file.originalname));
+    }
+})
+
+const upload = multer({storage})
+app.get("/", (req, res)=>{
+    res.render("index");
+})
+app.post ("/upload",upload.single("file"), (req, res)=>{
+    res.send("Arquivo recebido");
+})
+
+app.listen(8080, ()=>{
+    console.log("servidor rodando");
+})
